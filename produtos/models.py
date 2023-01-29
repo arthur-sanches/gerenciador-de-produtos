1363a80a0c43ad3b1e9bbd194383dfd580dc5211
Arthur Sanches <arthur.sanches96@gmail.com>
2023-01-29 19:37:23 -0300

    Adiciona model Produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=80)
+    sku = models.CharField(max_length=64, unique=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    descricao = models.TextField(default='')
+    datahora_adicionado = models.DateTimeField(auto_now_add=True)
+    datahora_modificado = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
