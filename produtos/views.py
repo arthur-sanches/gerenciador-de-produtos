e9ed79ff588e3ab374152221324fab14abdb3d77
Arthur Sanches <arthur.sanches96@gmail.com>
2023-01-30 17:09:08 -0300

    Refatora código dos filtros e os separa em funções

--- produtos/views.py
+++ produtos/views.py
@@ -13,7 +13,10 @@ from .serializers import ProdutoSerializer, CSVUploadSerializer
 
 
 def _trata_preco(preco):
-        return Decimal(preco.replace(',', '.'))
+    return Decimal(preco.replace(',', '.'))
+
+def _preco_eh_positivo(preco):
+    return True if preco > 0.00 else False
 
 
 class ProdutoList(generics.ListCreateAPIView):
@@ -61,18 +64,14 @@ class ProdutoExportCSV(generics.ListAPIView):
         preco_maior_param = self.request.query_params.get('preco_maior')
         preco_menor_param = self.request.query_params.get('preco_menor')
         descricao_param = self.request.query_params.get('descricao')
-        if nome_param is not None:
-            queryset = queryset.filter(nome__contains=nome_param)
-        if sku_param is not None:
-            queryset = queryset.filter(sku=sku_param)
-        if preco_param is not None:
-            queryset = queryset.filter(preco=_trata_preco(preco_param))
-        if preco_maior_param is not None:
-            queryset = queryset.filter(preco__gte=Decimal(_trata_preco(preco_maior_param)))
-        if preco_menor_param is not None:
-            queryset = queryset.filter(preco__lte=Decimal(_trata_preco(preco_menor_param)))
-        if descricao_param is not None:
-            queryset = queryset.filter(descricao__contains=descricao_param)
+        
+        queryset = self.filtro_nome(queryset, nome_param)
+        queryset = self.filtro_sku(queryset, sku_param)
+        queryset = self.filtro_preco(queryset, preco_param)
+        queryset = self.filtro_preco_maior(queryset, preco_maior_param)
+        queryset = self.filtro_preco_menor(queryset, preco_menor_param)
+        queryset = self.filtro_descricao(queryset, descricao_param)
+
         return queryset
 
     def get(self, requisicao):
@@ -87,4 +86,43 @@ class ProdutoExportCSV(generics.ListAPIView):
 
         resultados.to_csv(path_or_buf=resposta,sep=';',float_format='%.2f',index=False,decimal=",")
         return resposta
-        
+
+    def _valida_preco(self, preco):
+        if preco is not None:
+            preco_tratado = Decimal(_trata_preco(preco))
+            if _preco_eh_positivo(preco_tratado):
+                return preco_tratado
+        return False
+
+    def filtro_nome(self, queryset, nome_param):
+        if nome_param is not None:
+            return queryset.filter(nome__contains=nome_param)
+        return queryset
+    
+    def filtro_sku(self, queryset, sku_param):
+        if sku_param is not None:
+            return queryset.filter(sku=sku_param)
+        return queryset
+    
+    def filtro_preco(self, queryset, preco_param):
+        preco_validado = self._valida_preco(preco_param)
+        if preco_validado:
+            return queryset.filter(preco=preco_validado)
+        return queryset
+    
+    def filtro_preco_maior(self, queryset, preco_maior_param):
+        preco_validado = self._valida_preco(preco_maior_param)
+        if preco_validado:
+            return queryset.filter(preco__gte=preco_validado)
+        return queryset
+    
+    def filtro_preco_menor(self, queryset, preco_menor_param):
+        preco_validado = self._valida_preco(preco_menor_param)
+        if preco_validado:
+            return queryset.filter(preco__lte=preco_validado)
+        return queryset
+
+    def filtro_descricao(self, queryset, descricao_param):
+        if descricao_param is not None:
+            return queryset.filter(descricao__contains=descricao_param)
+        return queryset
